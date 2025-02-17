d7b40654d5ce273faa255cd55843cabc7b4a35ce
oxa10 <Oxa10@pct.edu>
2025-02-17 13:47:51 -0500

    midway

--- Lab # Your Name.py
+++ Lab # Your Name.py
@@ -15,5 +15,21 @@ import tkinter as tk
 
 # Create the main window
 root = tk.Tk()
-root.title("Mom GUI")  # Set window title
+root.title("Homework 2")  # Set window title
 root.attributes('-fullscreen', True)  # Set to full-screen mode
+
+def exit_fullscreen(event=None):
+    """
+    Exits full-screen mode when the Escape key is pressed.
+    """
+    root.attributes('-fullscreen', False)
+
+# -- Problem 1 -- Scaring the User
+def Creepy():
+
+# Bind the Escape key to exit full screen
+root.bind("<Escape>", exit_fullscreen)
+
+# Run the GUI loop
+root.mainloop()
+
