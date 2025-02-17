44ae17c72f635dccb882aa1204170a4a54e710d3
oxa10 <Oxa10@pct.edu>
2025-02-17 13:36:45 -0500

    Starting

--- Lab # Your Name.py
+++ Lab # Your Name.py
@@ -10,35 +10,10 @@ def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 install("pyvisa")
 
-# Import libraries.
-import pyvisa
+# Import tkinter
+import tkinter as tk
 
-rm = pyvisa.ResourceManager()
-
-#Find power supply address
-try:
-    Powersupply = [a for a in rm.list_resources() if 'SPD' in a]
-    supply = rm.open_resource(Powersupply[0])
-except IndexError:
-    print("PowerSupply not connected or powered on")
-
-#Find DMM address
-try:
-    Digital = [b for b in rm.list_resources() if 'SDM' in b]
-    dmm = rm.open_resource(Digital[0])
-except IndexError:
-    print("Digital MultiMeter not connected or powered on")
-
-#Find Osilly address
-try:
-    OSilly = [c for c in rm.list_resources() if 'SDS' in c]
-    oscope = rm.open_resource(OSilly[0])
-except IndexError:
-    print("Oscilloscope not connected or powered on")
-
-#Find Function  address
-try:
-    Fuci = [d for d in rm.list_resources() if 'SDG' in d]
-    fungen = rm.open_resource(Fuci[0])
-except IndexError:
-    print("Function Generator not connected or powered on")
+# Create the main window
+root = tk.Tk()
+root.title("Mom GUI")  # Set window title
+root.attributes('-fullscreen', True)  # Set to full-screen mode
